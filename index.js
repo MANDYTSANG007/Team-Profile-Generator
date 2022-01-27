3ede9a07c22695d8c7cc2a5481c9ea13c4a8c0db
Mandy Tsang <mandy.tsang007@gmail.com>
2022-01-26 23:28:56 -0800

    add engineer questions

--- index.js
+++ index.js
@@ -32,3 +32,27 @@ const newManager = [
     },
 ];
 
+const newEngineer = [
+    {
+        type: "input",
+        message: "What is the engineer's name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is the engineer's ID?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "What is the engineer's email?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is the engineer's GitHub username ?",
+        name: "github"
+    },
+];
+
+
