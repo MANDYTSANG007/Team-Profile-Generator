73545dacbf0f3728311ad210af1c2a1a153fd3f4
Mandy Tsang <mandy.tsang007@gmail.com>
2022-01-26 23:30:58 -0800

    add intern's questions

--- index.js
+++ index.js
@@ -55,4 +55,26 @@ const newEngineer = [
     },
 ];
 
+const newIntern = [
+    {
+        type: "input",
+        message: "What is the intern's name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is the intern's ID?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "What is the intern's email?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is the intern's school?",
+        name: "school"
+    },
+]
 
