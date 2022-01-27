5bf172e8bd215454ca0f5bf400d5c0146f8a1f31
Mandy Tsang <mandy.tsang007@gmail.com>
2022-01-26 23:25:02 -0800

    add questions to manager

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 const fs = requirer("fs");
 const inquirer = requirer("inquirer");
+
+const Manager = require(".lib/Manager");
+const Engineer = require(".lib/Engineer");
+const Intern = require(".lib/Intern");
+
+//store employee objects as they are created
+const employees = [];
+
+// inquirer questions
+const newManager = [
+    {
+        type: "input",
+        message: "What is the manager's name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is the manager's ID?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "What is the manager's email?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is the manager's office number?",
+        name: "officeNumber"
+    },
+];
+
